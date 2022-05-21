a133b22ad1028fa0e3c4a900ecf9e873f1550df3
sypersav <81754734+sypersav@users.noreply.github.com>
2022-05-21 14:49:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup() {
+    canvas = createCanvas(600, 600)
+    canvas.center()
+    video = createCapture(600, 600)
+    video.hide()
+}
+
+function draw() {
+
+}
+
+function preload() {
+
+}
